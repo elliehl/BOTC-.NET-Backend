f4350cfa6eef88d9a01159d4d05eced193a4bbb0
Ellie <101657425+elliehl@users.noreply.github.com>
2023-09-10 20:00:22 +0100

    Add responses

---
+++ BOTC/Models/Responses/AddGameResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BOTC.Models
+{
+	public class AddGameResponse: BaseGameDTO
+	{
+        public int Id { get; set; }
+        public string Starting_Role { get; set; }
+        public string Final_Role { get; set; }
+    }
+}
+
