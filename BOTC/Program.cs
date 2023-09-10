b7f4b02d308616a33d3d77b85e4a5954bd290d8f
Ellie <101657425+elliehl@users.noreply.github.com>
2023-09-10 18:47:40 +0100

    Add functionality for stats

--- BOTC/Program.cs
+++ BOTC/Program.cs
@@ -10,11 +10,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IContext, Context>();
 builder.Services.AddScoped<IGamesRepository, GamesRepository>();
 builder.Services.AddScoped<IGamesService, GamesService>();
+builder.Services.AddScoped<IStatsRepository, StatsRepository>();
+builder.Services.AddScoped<IStatsService, StatsService>();
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// This stops the CORS error that arises from having my front end in VS Code and back end in Visual Studio.
+// Used along with the UseCors line below
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("allow-botc",
+       builder =>
+       {
+           builder.WithOrigins("http://localhost:3000")
+           .AllowAnyHeader()
+           .AllowAnyMethod();
+       });
+});
 
 var app = builder.Build();
 
@@ -31,5 +46,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseCors("allow-botc");
 app.Run();
 
